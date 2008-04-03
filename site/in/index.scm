0c2765c7b3bd4635a4a7825044a2c8ce434e12e0
Francesco Salvestrini <salvestrini@gmail.com>
2008-04-03 19:35:51 +0200

    Updated site related file

--- site/in/index.scm
+++ site/in/index.scm
@@ -1,15 +1,51 @@
 (h3 "DESCRIPTION")
-(p "Embeddable micro kernel.")
-(h3 "AUTHORS")
-(p "Francesco Salvestrini <salvestrini AT gmail DOT com>")
-(h3 "RELEASE")
-(p "Sorry, no public release available at the moment.")
-(h3 "DEVELOPMENT")
-(p "You can Browse the "
-   (a (@ (href "http://git.savannah.nongnu.org/gitweb/?p=hybrid.git"))
-      "Git repository")
-   " of this project with your web browser. This gives you a good
-    picture of the current status of the source files. You may also view
-    the complete histories of any file in the repository as well as
-    differences among two versions."
+(p "Hybrid is a micro-kernel.")
+
+(h3 "MAINTAINERS")
+(p
+ "Francesco Salvestrini <salvestrini AT gmail DOT com>"
  )
+
+(h3 "AUTHORS")
+(p "Francesco Salvestrini <salvestrini AT gmail DOT com>"
+ )
+
+(h3 "RELEASES")
+(p "Sorry, no public release available at the moment.")
+
+(h3 "MAILING LISTS")
+(p
+ "Hybrid has several moderated mailing lists, each with an archive.
+ For general Hybrid discussions, use <hybrid-user AT nongnu.org>.
+ Email bug reports to <hybrid-bug AT nongnu.org>. For more information on
+ submitting bugs, please see the section Report a Bug below.
+ If you have a patch for a bug in Hybrid that hasn't yet been fixed in the
+ latest repository sources, please send the patch (made for the git sources,
+ not the release sources) to <hybrid-patch AT nongnu.org>."
+ )
+
+(h3 "REPORT A BUG")
+(p
+ "If you think you have found a bug in Hybrid, then please send as complete
+ a report as possible to <hybrid-bug AT nongnu.org>. An easy way to collect all
+ the required information, such as platform and compiler, is to run make check,
+ and include the resulting file tests/testsuite.log to your report.
+ Disagreements between the manual and the code are also bugs."
+ )
+
+(h3 "DEVELOPMENT")
+(h4 "Browsing sources")
+(p
+ "You can Browse the "
+ (a (@ (href "http://git.savannah.nongnu.org/gitweb/?p=hybrid.git"))
+    "Git repository")
+ " of this project with your web browser. This gives you a good
+ picture of the current status of the source files. You may also view
+ the complete histories of any file in the repository as well as
+ differences among two versions."
+ )
+(h4 "Getting a Copy of the Git Repository")
+(p "Anonymous checkout:")
+(br)
+(p (a (@ (href "http://savannah.gnu.org/maintenance/UsingGit")) git)
+   " clone git://git.savannah.nongnu.org/hybrid.git")
