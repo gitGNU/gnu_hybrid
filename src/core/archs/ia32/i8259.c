1826d0b094480fd76b3345713054fc733d773905
Francesco Salvestrini <salvestrini@gmail.com>
2008-05-10 00:31:20 +0200

    Fixed i8259 handling code

--- src/core/archs/ia32/i8259.c
+++ src/core/archs/ia32/i8259.c
@@ -30,42 +30,37 @@
 #define dprintf(F,A...)
 #endif
 
-#define SLAVE_IRQ    8
-#define MASTER_SLAVE 2
 #define ICU0         0x20
 #define ICU1         0xA0
-#define ICU_RESET    0x11
+#define ICU_RESET    0x11 /* 8086 mode + NOT single controller + call address */
 
-static uint16_t irq_mask;
+#define PIC_MASTER   ICU0
+#define PIC_SLAVE    ICU1
 
-static int remap(uint8_t master,
-		 uint8_t slave)
+static void remap(void)
 {
-	dprintf("Reprogramming master 0x%x / slave 0x%x\n");
+	dprintf("Remapping PIC\n");
 
 	/* Send ICW1 */
-	port_out8(ICU0,     ICU_RESET);
-	port_out8(ICU1,     ICU_RESET);
+	port_out8(PIC_MASTER,     ICU_RESET);
+	port_out8(PIC_SLAVE,      ICU_RESET);
 
-	/* ICW2: Send the PIC indices in the IDT */
-	port_out8(ICU0 + 1, master);
-	port_out8(ICU1 + 1, slave);
+	/* Send ICW2: controller base address */
+	port_out8(PIC_MASTER + 1, 0x20);
+	port_out8(PIC_SLAVE + 1,  0x28);
 
-	/* ICW3: Sets which PIC is the master */
-	port_out8(ICU0 + 1, 0x04);
-	port_out8(ICU1 + 1, 0x02);
+	/* Send ICW3 master: mask where slave is connected to master */
+	port_out8(PIC_MASTER + 1, 0x04);
+	/* Send ICW3 slave: index where the slave is connected on master */
+	port_out8(PIC_SLAVE + 1,  0x02);
 
-	/* If the lowest bit is set then the PIC is working in the x86 arch */
-	port_out8(ICU0 + 1, 0x01);
-	port_out8(ICU1 + 1, 0x01);
+	/* Send ICW4: 8086 mode, fully nested, not bufferd, no implicit EOI */
+	port_out8(PIC_MASTER + 1, 0x01);
+	port_out8(PIC_SLAVE + 1,  0x01);
 
-	/* Disable all IRQs */
-	port_out8(ICU0 + 1, 0xFF);
-	port_out8(ICU1 + 1, 0xFF);
-
-	irq_mask = 0xFFFF;
-
-	return 1;
+	/* Send OCW1: Disable all IRQs except the cascade */
+	port_out8(PIC_MASTER + 1, 0xFB);
+	port_out8(PIC_SLAVE + 1,  0xFF);
 }
 
 int i8259_init(void)
@@ -74,9 +69,9 @@ int i8259_init(void)
 
 	cli();
 
-	irq_mask = 0;
+	remap();
 
-	return remap(0x20, 0x28);
+	return 1;
 }
 
 void i8259_fini(void)
@@ -90,41 +85,44 @@ void i8259_irq_enable(int irq)
 {
 	dprintf("Enabling irq %d\n", irq);
 
-	irq_mask = irq_mask & ~(1 << irq);
-	if (irq >= 8) {
-		irq_mask = irq_mask & (1 << 2);
+	if (irq < 8) {
+		/* IRQ on master PIC */
+		port_out8(PIC_MASTER + 1,
+			  port_in8(PIC_MASTER + 1) & ~(1 << irq));
+	} else {
+		/* IRQ on slave PIC */
+		port_out8(PIC_SLAVE + 1,
+			  port_in8(PIC_SLAVE + 1)  & ~(1 << (irq - 8)));
 	}
-
-	port_out8(ICU0 + 1, irq_mask & 0xFF);
-	port_out8(ICU1 + 1, (irq_mask >> 8) & 0xFF);
 }
 
 void i8259_irq_disable(int irq)
 {
 	dprintf("Disabling irq %d\n", irq);
 
-	irq_mask = irq_mask | (1 << irq);
-	if ((irq_mask & 0xFF00) == 0xFF00) {
-		irq_mask = irq_mask | (1 << 2);
+	if (irq < 8) {
+		/* IRQ on master PIC */
+		port_out8(PIC_MASTER + 1,
+			  port_in8(PIC_MASTER + 1) | (1 << irq));
+	} else {
+		/* IRQ on slave PIC */
+		port_out8(PIC_SLAVE + 1,
+			  port_in8(PIC_SLAVE + 1)  | (1 << (irq - 8)));
 	}
-
-	port_out8(ICU0 + 1, irq_mask & 0xFF);
-	port_out8(ICU1 + 1, (irq_mask >> 8) & 0xFF);
 }
 
 uint16_t i8259_irq_mask_get(void)
 {
-	return irq_mask;
+	return (port_in8(PIC_SLAVE + 1) << 8 |
+		port_in8(PIC_MASTER + 1));
 }
 
 void i8259_irq_mask_set(uint16_t mask)
 {
 	dprintf("Setting irq mask to 0x%x\n", mask);
 
-	irq_mask = mask;
-
-	port_out8(ICU0 + 1, irq_mask & 0xFF);
-	port_out8(ICU1 + 1, (irq_mask >> 8) & 0xFF);
+	port_out8(PIC_MASTER + 1, (mask & 0x00FF));
+	port_out8(PIC_SLAVE + 1,  (mask & 0xFF00) >> 8);
 }
 
 static int irqs_enabled;
@@ -146,7 +144,7 @@ void arch_irqs_disable(void)
 	irqs_enabled = 0;
 }
 
-void arch_irqs_save(irq_flags_t* flags)
+void arch_irqs_save(irq_flags_t * flags)
 {
 	uint16_t mask;
 
@@ -155,7 +153,7 @@ void arch_irqs_save(irq_flags_t* flags)
 	mask = i8259_irq_mask_get();
 }
 
-void arch_irqs_restore(const irq_flags_t* flags)
+void arch_irqs_restore(const irq_flags_t * flags)
 {
 	unused_argument(flags);
 
