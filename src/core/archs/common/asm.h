346aa200309de8151da597246a97a1f5b8e4b0d5
Francesco Salvestrini <salvestrini@gmail.com>
2008-05-19 00:20:47 +0200

    Added common asm.h file

---
+++ src/core/archs/common/asm.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2008 Francesco Salvestrini
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#ifndef CORE_ARCHS_COMMON_ASM_H
+#define CORE_ARCHS_COMMON_ASM_H
+
+#include "config/config.h"
+
+/* C symbol format. HAVE_ASM_USCORE is defined by configure.  */
+#ifdef HAVE_ASM_USCORE
+# define EXT_C(sym)			_ ## sym
+#else
+# define EXT_C(sym)			sym
+#endif
+
+#endif /* CORE_ARCHS_COMMON_ASM_H */
