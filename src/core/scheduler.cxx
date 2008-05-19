2ea115d540f50a614b613ed0189ca64505e23e3c
Francesco Salvestrini <salvestrini@gmail.com>
2008-05-19 23:24:41 +0200

    Removed scheduler_lock() and scheduler_unlock() functions

--- src/core/scheduler.cxx
+++ src/core/scheduler.cxx
@@ -67,16 +67,6 @@ int scheduler_fini(void)
 	return 1;
 }
 
-void scheduler_lock(void)
-{
-	dprintf("Scheduler locked\n");
-}
-
-void scheduler_unlock(void)
-{
-	dprintf("Scheduler unlocked\n");
-}
-
 #if CONFIG_DEBUGGER
 static dbg_result_t command_processes_on_execute(FILE* stream,
 						 int   argc,
