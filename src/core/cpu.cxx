549eaf85500a6f6adda80bc26c9793d5a0f78926
Francesco Salvestrini <salvestrini@gmail.com>
2008-05-28 01:22:02 +0200

    Added CPUs core related sources

---
+++ src/core/cpu.cxx
@@ -0,0 +1,58 @@
+//
+// Copyright (C) 2008 Francesco Salvestrini
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+#include "config/config.h"
+#include "core/cpu.h"
+
+cpu::cpu()
+{
+}
+
+cpu::~cpu()
+{
+}
+
+void cpu::online()
+{
+	online_ = true;
+}
+
+void cpu::offline()
+{
+	online_ = false;
+}
+
+bool cpu::is_online()
+{
+	return online_;
+}
+
+cpus * cpus::instance_ = 0;
+
+cpus * cpus::instance()
+{
+	if (!instance_) {
+		instance_ = new cpus;
+	}
+
+	return instance_;
+}
+
+cpus::cpus()
+{
+}
---
+++ src/core/cpu.h
@@ -0,0 +1,52 @@
+//
+// Copyright (C) 2008 Francesco Salvestrini
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+#ifndef CORE_CPU_H
+#define CORE_CPU_H
+
+#include "config/config.h"
+#include "libc++/vector"
+
+class cpu {
+public:
+	cpu();
+	~cpu();
+
+	void online();
+	void offline();
+	bool is_online();
+
+private:
+	bool online_;
+};
+
+// Singleton for all the CPUs on the system
+class cpus {
+public:
+	static cpus * instance();
+
+protected:
+	cpus();
+	cpus(const cpus &);
+	cpus & operator= (const cpus &);
+
+private:
+	static cpus * instance_;
+};
+
+#endif /* CORE_CPU_H */
