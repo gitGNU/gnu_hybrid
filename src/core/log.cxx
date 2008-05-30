996506d48f7364ba7c6de30bd4666699e3619e86
Francesco Salvestrini <salvestrini@gmail.com>
2008-05-31 00:19:20 +0200

    Rearranged log related code as C++ sources

--- src/core/log.cxx
+++ src/core/log.cxx
@@ -1,21 +1,20 @@
-/*
- * Copyright (C) 2008 Francesco Salvestrini
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- */
+//
+// Copyright (C) 2008 Francesco Salvestrini
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
 
 #include "config/config.h"
 #include "libc/string.h"
@@ -26,6 +25,8 @@
 #include "core/dbg/debug.h"
 #include "core/log.h"
 #include "core/dbg/debugger/debugger.h"
+
+__BEGIN_DECLS
 
 #define BANNER          "log: "
 
@@ -235,3 +236,5 @@ DBG_COMMAND_DECLARE(log,
 		    command_log_on_execute,
 		    NULL);
 #endif
+
+__END_DECLS
