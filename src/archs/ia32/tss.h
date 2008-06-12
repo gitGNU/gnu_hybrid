4ed7b2a94827a79665d06bd5aff766815cf223d8
Francesco Salvestrini <salvestrini@gmail.com>
2008-06-12 18:49:41 +0200

    Re-added TSS related code

---
+++ src/archs/ia32/tss.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2008 Francesco Salvestrini
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#ifndef TSS_H
+#define TSS_H
+
+#include "config/config.h"
+#include "libc/stddef.h"
+
+__BEGIN_DECLS
+
+struct tss {
+	uint32_t backlink;
+        uint32_t esp0, ss0;
+        uint32_t esp1, ss1;
+        uint32_t esp2, ss2;
+        uint32_t cr3;
+        uint32_t eip;
+        uint32_t eflags;
+        uint32_t eax, ecx, edx, ebx;
+        uint32_t esp, ebp, esi, edi;
+        uint32_t es, cs, ss, ds, fs, gs;
+        uint32_t ldt;
+	uint16_t trace;
+	uint16_t iomapbase;
+} ATTRIBUTE(packed);
+typedef struct tss tss_t;
+
+int  tss_init(void);
+void tss_fini(void);
+
+__END_DECLS
+
+#endif /* TSS_H */
