6bc9141fabe38c8eb66202711184fb80c0f26afd
Francesco Salvestrini <salvestrini@gmail.com>
2008-07-21 21:12:17 +0200

    Rearranged pmm layer

--- src/mem/pmm.h
+++ src/mem/pmm.h
@@ -26,27 +26,8 @@
 
 __BEGIN_DECLS
 
-int    pmm_init(bootinfo_t * bi);
+int    pmm_init(void);
 void   pmm_fini(void);
-
-#define PMM_FLAG_VALID   0x01
-#define PMM_FLAG_USED    0x02
-#define PMM_FLAG_ENABLED 0x04
-#define PMM_FLAG_ALL     0xFF
-
-typedef uint8_t pmm_type_t;
-int    pmm_foreach(pmm_type_t flags,
-		   int        (* callback)(uint_t     start,
-					   uint_t     stop,
-					   pmm_type_t flags));
-uint_t pmm_reserve(uint_t start,
-		   uint_t stop);
-void   pmm_release(uint_t start);
-
-uint_t pmm_alloc(size_t size);
-void   pmm_free(uint_t start);
-
-uint_t pmm_page_alloc(size_t size);
 
 __END_DECLS
 
