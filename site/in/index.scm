b78eb3fb128ff437ea09544bf252544674443279
Francesco Salvestrini <salvestrini@gmail.com>
2008-09-21 17:24:04 +0200

    Updated main site page

--- site/in/index.scm
+++ site/in/index.scm
@@ -38,7 +38,7 @@
  (li "Debugging via RS232"))
 
 (h3 "COPYING")
-(p "Hybrid is licensed under the "
+(p "The project is licensed under the "
    (a (@ (href "http://www.gnu.org/licenses/licenses.html"))
       "GNU General Public License, version 2"))
 
@@ -52,22 +52,23 @@
 (p "Sorry, no public release available at the moment.")
 
 (h3 "MAILING LISTS")
-(p "Hybrid has a single moderated mailing list, with an archive. "
+(p "The project has a single moderated mailing list, with an archive. "
    "In order to post a message to the mailing list you must be subscribed. "
    "Please consult the "
    (a (@ (href "http://lists.nongnu.org/mailman/listinfo/hybrid-generic"))
-      "Hybrid mailing list page")
+      "mailing list page")
    " for more information on subscribing to the mailing list.")
 
 (h3 "REPORT A BUG")
-(p "If you think you have found a bug in Hybrid then please send as complete "
-   "a report as possible to <hybrid AT nongnu DOT org>. An easy way to collect "
-   "all the required information, such as platform and compiler, is to include "
-   "in your report the config.log file available at the end of the "
-   "configuration procedure. "
+(p "If you think you have found a bug then please send as complete a report "
+   "as possible to "
+   "<hybrid-generic AT nongnu DOT org>. "
+   "An easy way to collect all the required information, such as platform and "
+   "compiler, is to include in your report the config.log file available at "
+   "the end of the configuration procedure. "
    (br)
    (br)
-   "If you have a patch for a bug in Hybrid that hasn't yet been fixed in the "
+   "If you have a patch for a bug that hasn't yet been fixed in the "
    "latest repository sources, please be so kind to create it using the "
    "repository sources, not the release sources.")
 
@@ -83,7 +84,9 @@
 
 (h4 "Getting a copy of the Git Repository")
 (p "Anonymous checkout: "
-   (kbd (a (@ (href "http://savannah.gnu.org/maintenance/UsingGit")) git)
+   (br)
+   (br)
+   (kbd (a (@ (href "http://savannah.gnu.org/maintenance/UsingGit")) "git")
 	" clone git://git.savannah.nongnu.org/hybrid.git"))
 
 (h4 "Contribute")
