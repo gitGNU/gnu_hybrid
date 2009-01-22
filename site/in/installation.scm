cc366e41e49ad70dd884ba6f75718d72cc567d46
Francesco Salvestrini <salvestrini@gmail.com>
2009-01-22 23:00:38 +0100

    Added installation site related page

---
+++ site/in/installation.scm
@@ -0,0 +1,9 @@
+(h2 "INSTALLATION")
+
+(h3 "Prerequisites")
+(p "In order to install " ,as_site_variable_PROGRAMNAME " into your system "
+   "you will need:"
+   (ul
+    (li "A relatively recent GNU toolchain")
+    )
+   )
