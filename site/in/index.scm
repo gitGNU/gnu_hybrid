1bbe8c601e3fa7c78e2e7b264e910efebe5de770
Francesco Salvestrini <salvestrini@gmail.com>
2009-01-22 23:00:50 +0100

    Updated index site page

--- site/in/index.scm
+++ site/in/index.scm
@@ -43,9 +43,6 @@
 (h2 "AUTHORS")
 (p "Francesco Salvestrini <salvestrini AT gmail DOT com>")
 
-(h2 "RELEASES")
-(p "Sorry, no public release available at the moment.")
-
 (h2 "MAILING LISTS")
 (p "The project has a single moderated mailing list, with an archive. "
    "In order to post a message to the mailing list you must be subscribed. "
@@ -64,31 +61,3 @@
 (p "If you have a patch for a bug that hasn't yet been fixed in the "
    "latest repository sources, please be so kind to create it using the "
    "repository sources, not the release sources.")
-
-(h2 "DEVELOPMENT")
-
-(h3 "Browsing sources")
-(p "You can browse the "
-   (a (@ (href "http://git.savannah.nongnu.org/gitweb/?p=hybrid.git"))
-      "Git repository")
-   " of this project with your web browser. This gives you a good "
-   "picture of the current status of the source files. You may also view "
-   "the complete histories of any file in the repository as well as "
-   "differences among two versions.")
-
-(h3 "Getting a copy of the Git Repository")
-(p "Anonymous checkout:")
-(pre (@ class "terminal")
-     (a (@ (href "http://savannah.gnu.org/maintenance/UsingGit")) "git")
-     " clone git://git.savannah.nongnu.org/hybrid.git")
-
-(h3 "Contribute")
-(p "If you have time and programming skills, you can help us by developing "
-   "missing features, regression tests or bug-fixing the present codebase. "
-   "Subscribe to the "
-   (a (@ (href "http://lists.nongnu.org/mailman/listinfo/hybrid-generic"))
-      "mailing list")
-   ", drop us a mail and start coding. Send your code to the "
-   "mailing list under the form of patches for the current revision system.")
-(p "If you have time but no programming skills, you can help with "
-   "documentation,  packaging, tests before releases etc ...")
