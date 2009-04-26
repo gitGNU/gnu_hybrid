24a79064d7167223d06d514c37b8d7089bda7b8d
Francesco Salvestrini <salvestrini@gmail.com>
2009-04-26 23:04:52 +0200

    Added task header

---
+++ kernel/src/core/task.h
@@ -0,0 +1,66 @@
+//
+// Copyright (C) 2008, 2009 Francesco Salvestrini
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+#ifndef CORE_TASK_H
+#define CORE_TASK_H
+
+#include "config/config.h"
+#include "libc/stdint.h"
+#include "libc++/string"
+
+class task {
+public:
+        typedef uint_t id_t;
+
+        typedef enum {
+                INITIALIZING,
+                RUNNING,
+                PENDING,
+                READY
+        } state_t;
+
+	task(task::id_t          id,
+             const ktl::string & name) :
+                id_(id),
+                state_(INITIALIZING),
+                name_(name) { }
+
+	virtual ~task() { }
+
+        task::id_t id() const {
+                return id_;
+        };
+        const ktl::string & name() const {
+                return name_;
+        }
+
+        task::state_t state(void) const {
+                return state_;
+        }
+
+protected:
+	task();
+
+        task::id_t   id_;
+        state_t      state_;
+	ktl::string  name_;
+
+private:
+};
+
+#endif // CORE_TASK_H
