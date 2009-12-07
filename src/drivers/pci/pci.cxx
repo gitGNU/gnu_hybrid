b3731ac9b648c13ab439f862e1e23f60b52b7db4
Francesco Salvestrini <salvestrini@gmail.com>
2009-12-07 01:05:11 +0100

    Added PCI related directory

---
+++ src/drivers/pci/pci.cxx
@@ -0,0 +1,46 @@
+//
+// Copyright (C) 2008, 2009 Francesco Salvestrini
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+#include "config/config.h"
+#include "archs/arch.h"
+#include "libc/stdint.h"
+#include "libc/stdio.h"
+#include "libc/stddef.h"
+#include "libs/debug.h"
+#include "dbg/debugger.h"
+#include "libc++/string"
+#include "drivers/bus.h"
+
+#define BANNER          "pci: "
+
+#if CONFIG_BUS_DEBUG
+#define dprintf(F,A...) printf(BANNER F,##A)
+#else
+#define dprintf(F,A...)
+#endif
+
+pci::pci(const std::string & name) :
+        bus(name)
+{
+        dprintf("Bus 0x%x created (name = '%s')\n", this, name_.c_str());
+}
+
+pci::~pci()
+{
+        dprintf("Bus 0x%x (name = '%s') destroyed\n", this, name_.c_str());
+}
